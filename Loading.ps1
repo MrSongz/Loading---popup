1c73cc83db083fc75d310162b8e3baa8cb570132
MrSongz <39522843+MrSongz@users.noreply.github.com>
2018-05-24 09:31:19 +0200

    Add files via upload

---
+++ Loading.ps1
@@ -0,0 +1,19 @@
+﻿$texte = @("|","/","-","\")
+$texte2 = "loading"
+$texte3 = @("",".","..","...")
+while(1)
+{
+    $i=0
+    while($i -ne $texte2.Length)
+    {
+        cls
+
+        Write-Host "["$texte[($i%($texte.Length))]"]  " -NoNewline
+        Write-Host $texte2.Substring(0,$i) -NoNewline
+        Write-Host ($texte2.Substring($i,1)).ToUpper() -NoNewline
+        Write-Host $texte2.Substring($i+1,($texte2.Length-$i-1)) -NoNewline
+        Write-Host $texte3[$i%($texte3.Length)]
+        $i++
+        sleep -Milliseconds 200
+    }
+}
---
+++ popUp.ps1
@@ -0,0 +1,15 @@
+﻿cls
+
+[string]$Titre = Read-Host("Quel est le titre de la pop up ?")
+[string]$Message = Read-Host("Quel est le texte du message ?")
+[int]$typeBouton = Read-Host("Quel est le type de Bouton ?`n`n[1] - Ok/Cancel`n[2] - Abort/Retry/Ignore`n[3] - Yes/No/Cancel`n[4] - Yes/No`n[5] - Retry/Cancel`n")
+[int]$Icone = Read-Host("Quel est le type d'icone ?`n`n[16] - Erreur`n[32] - Question`n[48] - Avertissement`n[64] - Information`n")
+    
+
+# Charger la biblithèque d'objets graphiques Windows.Forms
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+
+# Afficher la boite de dialogue et renvoyer la valeur de retour (bouton appuyé)
+$Reponse = [System.Windows.Forms.MessageBox]::Show($Message, $Titre , $typeBouton, $Icone)
+
+#[System.Windows.Forms.MessageBox]::Show("hello", "bonjour" ,3, 32)
